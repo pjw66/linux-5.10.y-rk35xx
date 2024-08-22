72f8f7d6a9e939529684e821a956ee8047ab6800
pjw66 <116270063+pjw66@users.noreply.github.com>
2024-08-22 08:52:05 +0000

    add hejiaqin

--- drivers/net/wireless/rtl8723ds/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/rtl8723ds/os_dep/linux/os_intfs.c
@@ -1377,7 +1377,7 @@ int rtw_os_ndev_register(_adapter *adapter, const char *name)
 
 
 #ifdef CONFIG_RTW_NAPI
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 0)
 	netif_napi_add(ndev, &adapter->napi, rtw_recv_napi_poll, RTL_NAPI_WEIGHT);
 #else
 	netif_napi_add(ndev, &adapter->napi, rtw_recv_napi_poll);

04a9cf7a25ef3b3957678aa091fbaeb7ccb9222b
pjw66 <116270063+pjw66@users.noreply.github.com>
2024-08-23 02:36:22 +0000

    rtl8723ds KERNEL_VERSION change

--- drivers/net/wireless/rtl8723ds/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/rtl8723ds/os_dep/linux/ioctl_cfg80211.c
@@ -4208,7 +4208,7 @@ static int cfg80211_rtw_change_beacon(struct wiphy *wiphy, struct net_device *nd
 	return ret;
 }
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
 static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev, unsigned int link_id)
 #else
 static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev)
